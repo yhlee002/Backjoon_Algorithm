b8429495de04c419a3158ea98bc9c4b37b9f892e
yhlee <xxxoxxo002@gmail.com>
2022-07-16 00:45:57 +0900

    이진 탐색 문제 추가(2805, 1920)

--- BinarySearch/Algorithm1920.java
+++ BinarySearch/Algorithm1920.java
@@ -1,2 +1,48 @@
-package BinarySearch;public class Algorithm1920 {
+package BinarySearch;
+
+import java.util.*;
+
+// 수 찾기
+public class Algorithm1920 {
+    private List<Integer> solution(int[] arr1, int[] arr2) {
+        List<Integer> answer = new ArrayList<Integer>();
+
+        Arrays.sort(arr1);
+        for (int i = 0; i < arr2.length; i++) {
+            int lt = 0, cnt = 0;
+            int rt = arr1.length - 1;
+            while(lt <= rt) {
+                int mid = (lt + rt) / 2;
+
+                if (arr2[i] < arr1[mid]) rt = mid - 1;
+                else if(arr2[i] > arr1[mid]) lt = mid + 1;
+                else {
+                    cnt++;
+                    break;
+                }
+            }
+            answer.add(cnt);
+        }
+
+
+        return answer;
+    }
+
+
+
+    public static void main(String[] args) {
+        Algorithm1920 main = new Algorithm1920();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr1 = new int[n];
+        for (int i = 0; i < n; i++) arr1[i] = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr2 = new int[k];
+        for(int i = 0; i < k; i++) arr2[i] = sc.nextInt();
+
+        for (int x : main.solution(arr1, arr2)) System.out.println(x);
+
+
+    }
 }
--- BinarySearch/Algorithm2805.java
+++ BinarySearch/Algorithm2805.java
@@ -1,4 +1,40 @@
+package BinarySearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 // 나무 자르기
 public class Algorithm2805 {
+    private int solution(int n, int m, int[] list) {
+        int answer = 0;
+
+        int lt = 1;
+        int rt = Arrays.stream(list).max().getAsInt();
+
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            long sum = 0;
+            for (int x : list) {
+                if (x - mid > 0) sum += (x - mid);
+            }
+            if (sum >= m) {
+                lt = mid + 1;
+                answer = mid;
+            }
+            else rt = mid - 1;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algorithm2805 main = new Algorithm2805();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] list = new int[n];
+        for (int i = 0; i < n; i++) list[i] = sc.nextInt();
+
+        System.out.println(main.solution(n, m, list));
+    }
 }
