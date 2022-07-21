a37bad73646c41f40794842bdab4f5ae4582366f
yhlee <xxxoxxo002@gmail.com>
2022-07-21 23:43:16 +0900

    이진 탐색(two-pointer) 문제 추가(2470)

---
+++ BinarySearch/Algorithm2470.java
@@ -0,0 +1,41 @@
+package BinarySearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
+// 두 용액
+public class Algorithm2470 {
+    private void solution(int n, int[] list) {
+        int min = Integer.MAX_VALUE;
+
+        int minVal = 0, minVal2 = 0;
+        int lt = 0, rt = n - 1;
+        Arrays.sort(list);
+
+        while(lt < rt) {
+            int sum = list[lt] + list[rt];
+
+            if (Math.abs(sum) < min) {
+                min = Math.abs(sum);
+                minVal = list[lt];
+                minVal2 = list[rt];
+            }
+
+            if (sum > 0) rt -= 1;
+            else lt += 1;
+        }
+
+        System.out.print(minVal + " " + minVal2);
+    }
+
+    public static void main(String[] args) {
+        Algorithm2470 main = new Algorithm2470();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        main.solution(n, arr);
+    }
+}
+
