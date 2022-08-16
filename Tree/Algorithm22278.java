c980d27f8c6b010335ebae6139bbbad1563b570d
yhlee <xxxoxxo002@gmail.com>
2022-08-17 00:56:15 +0900

    트리 문제 추가(22278번 두 최단 경로) - 미완성, 수정 필요

---
+++ Tree/Algorithm22278.java
@@ -0,0 +1,81 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// 두 최단 경로
+public class Algorithm22278 {
+    static int n;
+    static int m;
+    static int[][] matrix;
+    static boolean[][] checked;
+    static List<Integer>[] paths;
+    static boolean[] checked2;
+    static int min;
+
+    void DFS(int x, int k, int sum) { // x: 목표지점, k: 현재 지점, sum: 경로 가중치의 합
+        if (k == x) { // 하나의 경로 탐색
+            paths[x - 1].add(sum);
+        } else {
+            for (int i = 1; i <= n; i++) {
+                if (!checked[k][i] && i != k) {
+                    checked[k][i] = true;
+                    DFS(x, i, sum + matrix[k][i]);
+                    checked[k][i] = false;
+                }
+            }
+        }
+    }
+
+    void getMinSum(int L, int x, int sum) {
+        if (L == 2) {
+            min = Math.min(min, sum);
+        } else {
+            for (int i = 0; i < paths[x - 1].size(); i++) {
+                if (!checked2[i]) {
+                    checked2[i] = true;
+                    getMinSum(L + 1, x, sum + paths[x - 1].get(i));
+                    getMinSum(L, x, sum);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Algorithm22278 main = new Algorithm22278();
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        for (int k = 0; k < tc; k++) {
+            n = sc.nextInt();
+            m = sc.nextInt();
+            paths = new ArrayList[n];
+            matrix = new int[n + 1][n + 1];
+            checked = new boolean[n + 1][n + 1];
+
+            for (int i = 1; i <= m; i++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                matrix[a][b] = sc.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (i > 0) {
+                    min = Integer.MAX_VALUE;
+                    paths[i] = new ArrayList<>();
+
+                    main.DFS(i + 1, 1, 0);
+                    if (paths[i].size() < 2) min = -1;
+                    else {
+                        checked2 = new boolean[paths[i].size()];
+                        main.getMinSum(0, i + 1,0);
+                    }
+
+                    System.out.print(min + " ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
